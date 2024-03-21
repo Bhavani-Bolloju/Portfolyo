7ec90334b5c8315b6427e962052a6860be516d1e
bhavani bolloju <bhavani.bolloju@gmail.com>
2024-03-21 12:27:50 +0530

    about section content

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,80 @@
 // import React from 'react'
 
+import classes from "./About.module.scss";
+
 function About() {
   return (
-    <div>About</div>
-  )
+    <section className={classes["about"]}>
+      <div className={classes["about__container"]}>
+        <div className={classes["about__inner"]}>
+          <div className={classes["about__left"]}>
+            <div className={classes["about__image"]}>
+              <img src="img/thumbs/1-1.jpg" alt="" />
+              <div className="main" data-img-url="img/about/2.jpg"></div>
+              <div className="experience">
+                <h3 className="year" data-img-url="img/about/2.jpg">
+                  7
+                </h3>
+                <div id="circle">
+                  <svg
+                    version="1.1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    xmlns:xlink="http://www.w3.org/1999/xlink"
+                    x="0px"
+                    y="0px"
+                    width="300px"
+                    height="300px"
+                    viewBox="0 0 300 300"
+                    enable-background="new 0 0 300 300"
+                    xml:space="preserve"
+                  >
+                    <defs>
+                      <path
+                        id="circlePath"
+                        d=" M 150, 150 m -60, 0 a 60,60 0 0,1 120,0 a 60,60 0 0,1 -120,0 "
+                      />
+                    </defs>
+                    <circle cx="150" cy="100" r="75" fill="none" />
+                    <g>
+                      <use xlink:href="#circlePath" fill="none" />
+                      <text fill="#000">
+                        <textpath xlink:href="#circlePath">
+                          Years of best and successful work experience
+                        </textpath>
+                      </text>
+                    </g>
+                  </svg>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={classes["about__right"]}>
+            <div className={classes["about__title"]}>
+              <span>About Me</span>
+              <h3>
+                A passionate Senior Designer and Front-End Developer based In
+                NYC, USA
+              </h3>
+            </div>
+            <div className={classes["about__text"]}>
+              <p>
+                I am passionate Senior Designer, and I'm very passionate and
+                dedicated to my work. With 7 years experience as a professional
+                Web developer, I have acquired the skills and knowledge
+                necessary to make your project a success.
+              </p>
+            </div>
+            <div className={classes["about__signature"]}>
+              <img src="img/signature.png" alt="" />
+            </div>
+            <div className={classes["about__button"]}>
+              <a href="#">Download CV</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default About
+export default About;
