d9afca0e3e22d4e3d5535ee3b6725b95ee836221
bhavani bolloju <bhavani.bolloju@gmail.com>
2024-03-21 12:16:55 +0530

    complete hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,8 +1,66 @@
 // import React from 'react'
 import classes from "./Hero.module.scss";
+import video1 from "../../assets/video/1.mp4";
+import html from "../../assets/img/svg/html.svg";
+import css from "../../assets/img/svg/css.svg";
+import bootstrap from "../../assets/img/svg/bootstrap.svg";
+import angular from "../../assets/img/svg/angular.svg";
+import avatar from "../../assets/img/about/4.jpg";
 
 function Hero() {
-  return <div className={classes.hero}>Hero</div>;
+  return (
+    <div className={classes.hero}>
+      <div className={classes["hero__container"]}>
+        <div className={classes["hero__content"]}>
+          <div className={classes["hero__texts"]}>
+            <div className={classes["hero__job"]}>
+              <video autoPlay loop muted width="100%" playsInline>
+                <source src={video1} type="video/mp4" />
+              </video>
+              <h3>
+                Senior Designer
+                <br />
+                &amp; Developer
+              </h3>
+            </div>
+            <p className={classes["hero__desc"]}>
+              Hi, I'm Alan Walker. A passionate Senior Designer and Front-End
+              Developer based In NYC, USA
+            </p>
+            <div className={classes["hero__short-skills"]}>
+              <div className={classes["hero__text"]}>
+                <span>
+                  High knowledge on
+                  <br />
+                  softwares
+                </span>
+              </div>
+
+              <div className={classes["hero__icons"]}>
+                <ul>
+                  <li>
+                    <img className="svg" src={html} alt="html" />
+                  </li>
+                  <li>
+                    <img className="svg" src={css} alt="css" />
+                  </li>
+                  <li>
+                    <img className="svg" src={angular} alt="angular" />
+                  </li>
+                  <li>
+                    <img className="svg" src={bootstrap} alt="bootstrap" />
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className={classes["hero__avatar"]}>
+            <img className={classes["hero__image"]} src={avatar}></img>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
