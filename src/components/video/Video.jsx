115773275cac83bf5d81626c6badd086d42e14fd
bhavani bolloju <bhavani.bolloju@gmail.com>
2024-03-22 12:10:26 +0530

    complete video section

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,8 +1,51 @@
-// import React from 'react'
+import { useState } from "react";
 
 import classes from "./Video.module.scss";
 
+const VideoOverlay = ({ onClose, link }) => {
+  return (
+    <div className={classes["overlay"]}>
+      <div className={classes["video-container"]}>
+        <button className={classes["close-button"]} onClick={onClose}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 18 18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+        <iframe
+          className={classes["video-frame"]}
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/VIDEO_ID"
+          title="YouTube video player"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
 function Video() {
+  const [showOverlay, setShowOverlay] = useState(true);
+
+  const openOverlay = () => {
+    setShowOverlay(true);
+  };
+
+  const closeOverlay = () => {
+    setShowOverlay(false);
+  };
+
   return (
     <section className={classes["video"]}>
       <div className={classes["vide__background"]}>
@@ -13,33 +56,34 @@ function Video() {
         >
           {/* <img src="" alt="" /> */}
         </div>
-        <div className={classes["video__overlay"]}></div>
       </div>
       <div className={classes["video__content"]}>
         <div className={classes["video__container"]}>
           <div className={classes["video__content_inner"]}>
-            <span className={classes["video__rounded"]}>
-              <a
-                className="vvideo__popup-youtube"
-                href="https://www.youtube.com/watch?v=ICr_bOuM9Zo"
-              ></a>
-            </span>
+            <button
+              onClick={openOverlay}
+              className={classes["video__rounded"]}
+            ></button>
+
             <h3 className={classes["video__text"]} data-wow-duration="1.5s">
               I am delivering beautiful digital products for my clients
             </h3>
-            <div
+            <button
               className={classes["video__button"]}
               data-wow-duration="1.5s"
               data-wow-delay="0.2s"
+              onClick={openOverlay}
             >
-              <a
-                className={classes["video__popup-youtube"]}
-                href="https://www.youtube.com/watch?v=ICr_bOuM9Zo"
-              >
-                Watch Video
-              </a>
-            </div>
+              Watch Video
+            </button>
           </div>
+
+          {showOverlay && (
+            <VideoOverlay
+              onClose={closeOverlay}
+              link="https://www.youtube.com/watch?v=ICr_bOuM9Zo"
+            />
+          )}
         </div>
       </div>
     </section>
