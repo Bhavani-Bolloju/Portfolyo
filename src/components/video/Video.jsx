cbee2fd9240a153350ad2ccf3798b39d97a16543
bhavani bolloju <bhavani.bolloju@gmail.com>
2024-03-22 10:00:30 +0530

    add video section  markup

--- src/components/video/Video.jsx
+++ src/components/video/Video.jsx
@@ -1,9 +1,49 @@
 // import React from 'react'
 
+import classes from "./Video.module.scss";
+
 function Video() {
   return (
-    <div>Video</div>
-  )
+    <section className={classes["video"]}>
+      <div className={classes["vide__background"]}>
+        <div
+          className={classes["video__image"]}
+          data-speed="0"
+          data-img-url="img/hero/1.jpg"
+        >
+          {/* <img src="" alt="" /> */}
+        </div>
+        <div className={classes["video__overlay"]}></div>
+      </div>
+      <div className={classes["video__content"]}>
+        <div className={classes["video__container"]}>
+          <div className={classes["video__content_inner"]}>
+            <span className={classes["video__rounded"]}>
+              <a
+                className="vvideo__popup-youtube"
+                href="https://www.youtube.com/watch?v=ICr_bOuM9Zo"
+              ></a>
+            </span>
+            <h3 className={classes["video__text"]} data-wow-duration="1.5s">
+              I am delivering beautiful digital products for my clients
+            </h3>
+            <div
+              className={classes["video__button"]}
+              data-wow-duration="1.5s"
+              data-wow-delay="0.2s"
+            >
+              <a
+                className={classes["video__popup-youtube"]}
+                href="https://www.youtube.com/watch?v=ICr_bOuM9Zo"
+              >
+                Watch Video
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
-export default Video
+export default Video;
